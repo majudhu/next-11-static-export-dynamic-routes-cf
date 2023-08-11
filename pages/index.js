fd809fc2f296d733dc394cf86f84c9b3fa308135
majudhu <majudhu.sup@gmail.com>
2023-08-11 11:40:40 +0500

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <p>
+        <Link href='/post/one'>Post one</Link>
+      </p>
+      <p>
+        <Link href='/post/two'>Post two</Link>
+      </p>
+      <p>
+        <Link href='/post/three'>Post three</Link>
+      </p>
+      <p>
+        <Link href='/post/four'>Post 4four</Link>
+      </p>
+    </div>
+  );
+}
+``;
---
+++ pages/post/[id].js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const { query } = useRouter();
+
+  return (
+    <div>
+      <h1>Post {query?.id}</h1>
+      <p>
+        {Array.from({ length: 20 }, (_, i) => i + 1)
+          .map(() => query?.id)
+          .join(' - ')}
+      </p>
+      <p>
+        <Link href='/'>Home</Link>
+      </p>
+    </div>
+  );
+}
+``;
